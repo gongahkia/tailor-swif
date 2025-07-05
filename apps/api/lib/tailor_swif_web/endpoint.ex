b6a917c4d56b22832fc49b93b1db5c93c464fdfd
gongahkia <angryapplegravy@gmail.com>
2025-07-05 11:10:42 +0800

    feat(app): sartorial elements

--- apps/api/lib/tailor_swif_web/endpoint.ex
+++ apps/api/lib/tailor_swif_web/endpoint.ex
@@ -1,40 +1,20 @@
 defmodule TailorSwifWeb.Endpoint do
-  use Phoenix.Endpoint, otp_app: :api
+  use Phoenix.Endpoint, otp_app: :tailor_swif
 
-  # The session will be stored in the cookie and signed,
-  # this means its contents can be read but not tampered with.
-  # Set :encryption_salt if you would also like to encrypt it.
-  @session_options [
-    store: :cookie,
-    key: "_api_key",
-    signing_salt: "A8vAzZ0p",
-    same_site: "Lax"
-  ]
+  socket "/socket", TailorSwifWeb.UserSocket,
+    websocket: true,
+    longpoll: false
 
-  socket "/live", Phoenix.LiveView.Socket,
-    websocket: [connect_info: [session: @session_options]],
-    longpoll: [connect_info: [session: @session_options]]
-
-  # Serve at "/" the static files from "priv/static" directory.
-  #
-  # You should set gzip to true if you are running phx.digest
-  # when deploying your static files in production.
   plug Plug.Static,
     at: "/",
-    from: :api,
+    from: :tailor_swif,
     gzip: false,
-    only: TailorSwifWeb.static_paths()
+    only: ~w(favicon.ico robots.txt)
 
-  # Code reloading can be explicitly enabled under the
-  # :code_reloader configuration of your endpoint.
   if code_reloading? do
     plug Phoenix.CodeReloader
-    plug Phoenix.Ecto.CheckRepoStatus, otp_app: :api
+    plug Phoenix.Ecto.CheckRepoStatus, otp_app: :tailor_swif
   end
-
-  plug Phoenix.LiveDashboard.RequestLogger,
-    param_key: "request_logger",
-    cookie_key: "request_logger"
 
   plug Plug.RequestId
   plug Plug.Telemetry, event_prefix: [:phoenix, :endpoint]
@@ -46,6 +26,6 @@ defmodule TailorSwifWeb.Endpoint do
 
   plug Plug.MethodOverride
   plug Plug.Head
-  plug Plug.Session, @session_options
+  plug Plug.Session, store: :cookie, key: "_tailor_swif_key", signing_salt: "randomsalt"
   plug TailorSwifWeb.Router
 end
