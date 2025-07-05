7bd0681cb5a874ff3fede830121f837f47dabbb5
gongahkia <angryapplegravy@gmail.com>
2025-07-05 11:10:24 +0800

    feat(app): recognising error prone

--- apps/api/lib/tailor_swif_web/controllers/error_json.ex
+++ apps/api/lib/tailor_swif_web/controllers/error_json.ex
@@ -1,21 +1,19 @@
 defmodule TailorSwifWeb.ErrorJSON do
-  @moduledoc """
-  This module is invoked by your endpoint in case of errors on JSON requests.
+  use TailorSwifWeb, :controller
 
-  See config/config.exs.
-  """
+  def render("404.json", _assigns) do
+    %{error: "Not Found"}
+  end
 
-  # If you want to customize a particular status code,
-  # you may add your own clauses, such as:
-  #
-  # def render("500.json", _assigns) do
-  #   %{errors: %{detail: "Internal Server Error"}}
-  # end
+  def render("500.json", _assigns) do
+    %{error: "Internal Server Error"}
+  end
 
-  # By default, Phoenix returns the status message from
-  # the template name. For example, "404.json" becomes
-  # "Not Found".
+  def render("422.json", %{errors: errors}) do
+    %{errors: errors}
+  end
+
   def render(template, _assigns) do
-    %{errors: %{detail: Phoenix.Controller.status_message_from_template(template)}}
+    %{error: Phoenix.Controller.status_message_from_template(template)}
   end
 end
